2a9004e834b4cf312e1517f09505ddaf0e17553f
Krzysztof <krzysiek@woelke.pl>
2022-11-18 16:56:48 +0100

    Init structure for this project

---
+++ src/main/java/pl/woelke/PasswordGeneratorapi/PasswordGeneratorApiApplication.java
@@ -0,0 +1,13 @@
+package pl.woelke.PasswordGeneratorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordGeneratorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordGeneratorApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/pl/woelke/PasswordGeneratorapi/config/LoginCredentials.java
@@ -0,0 +1,11 @@
+package pl.woelke.PasswordGeneratorapi.config;
+
+import lombok.Getter;
+
+@Getter
+public class LoginCredentials {
+
+    private String userName;
+    private String password;
+
+}
